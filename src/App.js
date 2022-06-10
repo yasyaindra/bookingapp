937cf88e0dc07acf76753a641d0827c8f1b674b0
yasyaindra <indrayasya96@gmail.com>
2022-06-10 14:21:48 +0800

    kickstarted

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home, List, Hotel } from "./pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
